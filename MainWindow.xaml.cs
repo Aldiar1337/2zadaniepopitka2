53783d86195764bf7b1e79bafd99826363fbba70
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 16:31:55 +0700

    Functia2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,39 +30,9 @@ namespace WpfApp1
             else
             {
                 MessageBox.Show("Пожалуйста, введите множитель.");
-            }
+            
+
+
+
         }
-        
-        
-
-        private void AddNumber_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(numberTextBox.Text))
-            {
-                if (int.TryParse(numberTextBox.Text, out int number))
-                {
-                    numbers.Add(number);
-                    UpdateListBox();
-                }
-                else
-                {
-                    MessageBox.Show("Пожалуйста, введите допустимое целое число».");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите число.");
-            }
-        }
-
-
-        private void UpdateListBox()
-        {
-            numberListBox.ItemsSource = null;
-            numberListBox.ItemsSource = numbers;
-        }
-
-        
-
-    }
 }
