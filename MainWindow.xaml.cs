b85295b0806700f754bb145dd4f8c2eb70c728d4
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 16:00:12 +0700

    1Functia

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        List<int> numbers = new List<int>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        // Помножить все числа на множитель
+        private void MultiplyByFactor_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(numberTextBox.Text))
+            {
+                if (int.TryParse(numberTextBox.Text, out int factor))
+                {
+                    numbers = numbers.Select(num => num * factor).ToList();
+                    UpdateListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите допустимый множитель.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите множитель.");
+            }
+        }
+        
+        private void UpdateListBox()
+        {
+            numberListBox.ItemsSource = null;
+            numberListBox.ItemsSource = numbers;
+        }
+    }
+}
