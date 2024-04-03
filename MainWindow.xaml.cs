d8fe7bf35cbf67503ce992cac239f88f5e167a53
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 17:19:15 +0700

    4Functia

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,16 @@ using System.Windows;
 
 namespace WpfApp1
 {
-   
+    public partial class MainWindow : Window
+    {
+        private void ClearList_Click(object sender, RoutedEventArgs e)
+        {
+            numbers.Clear();
+            UpdateListBox();
+        }
+
+        
+        
+    }
 }
 
