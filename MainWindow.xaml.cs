79fcc2c8a26e6a6e374f3c4d6e92fc937087213b
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 16:45:12 +0700

    Functia3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,69 +5,17 @@ using System.Windows;
 
 namespace WpfApp1
 {
-    public partial class MainWindow : Window
+    private void SortAscending_Click(object sender, RoutedEventArgs e)
     {
-        List<int> numbers = new List<int>();
+        numbers.Sort();
+        UpdateListBox();
+    }
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        // Помножить все числа на множитель
-        private void MultiplyByFactor_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(numberTextBox.Text))
-            {
-                if (int.TryParse(numberTextBox.Text, out int factor))
-                {
-                    numbers = numbers.Select(num => num * factor).ToList();
-                    UpdateListBox();
-                }
-                else
-                {
-                    MessageBox.Show("Пожалуйста, введите допустимый множитель.");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите множитель.");
-            }
-        }
-
-
-        private void AddNumber_Click(object sender, RoutedEventArgs e)
-
-        {
-            if (!string.IsNullOrEmpty(numberTextBox.Text))
-            {
-                if (int.TryParse(numberTextBox.Text, out int number))
-                {
-                    numbers.Add(number);
-                    UpdateListBox();
-                }
-                else
-                {
-                    MessageBox.Show("Пожалуйста, введите допустимое целое число».");
-                }
-            }
-            else
-            {
-
-                MessageBox.Show("Пожалуйста, введите множитель.");
-
-                MessageBox.Show("Пожалуйста, введите число.");
-
-            }
-        }
-
-
-        private void UpdateListBox()
-        {
-            numberListBox.ItemsSource = null;
-            numberListBox.ItemsSource = numbers;
-        }
-
-
-            }
-        }
+    // Отсортировать по убыванию
+    private void SortDescending_Click(object sender, RoutedEventArgs e)
+    {
+        numbers.Sort();
+        numbers.Reverse();
+        UpdateListBox();
+    }
+}
