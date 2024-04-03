bf4c85752e57735525e7e501760f34cf39b8e790
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 16:38:36 +0700

    Functia2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,32 +7,5 @@ namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
-        List<int> numbers = new List<int>();
-
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        private void MultiplyByFactor_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(numberTextBox.Text))
-            {
-                if (int.TryParse(numberTextBox.Text, out int factor))
-                {
-                    numbers = numbers.Select(num => num * factor).ToList();
-                    UpdateListBox();
-                }
-                else
-                {
-                    MessageBox.Show("Пожалуйста, введите допустимый множитель.");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите множитель.");
-            
-
-
-
-        }
+        
 }
