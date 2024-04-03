18f0a41294ad6403f238e8543cbfc98d659c1ed7
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 16:21:16 +0700

    Functia2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,34 @@ using System.Windows;
 
 namespace WpfApp1
 {
-   
+    public partial class MainWindow : Window
+    {
+        List<int> numbers = new List<int>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void AddNumber_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(numberTextBox.Text))
+            {
+                if (int.TryParse(numberTextBox.Text, out int number))
+                {
+                    numbers.Add(number);
+                    UpdateListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите допустимое целое число».");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите число.");
+            }
+        }
+
     }
 }
