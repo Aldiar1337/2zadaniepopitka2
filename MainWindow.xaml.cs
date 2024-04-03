92399ef967d322795958de99d2260ffce3bfe912
Aldiar1337 <160225737+Aldiar1337@users.noreply.github.com>
2024-04-03 16:40:55 +0700

    Revert "Functia2"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,27 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+        private void MultiplyByFactor_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(numberTextBox.Text))
+            {
+                if (int.TryParse(numberTextBox.Text, out int factor))
+                {
+                    numbers = numbers.Select(num => num * factor).ToList();
+                    UpdateListBox();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите допустимый множитель.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите множитель.");
+            }
+        }
 
+        
         private void AddNumber_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrEmpty(numberTextBox.Text))
@@ -34,14 +54,11 @@ namespace WpfApp1
             }
         }
 
-
         private void UpdateListBox()
         {
             numberListBox.ItemsSource = null;
             numberListBox.ItemsSource = numbers;
         }
 
-
-            }
-        }
     }
+}
