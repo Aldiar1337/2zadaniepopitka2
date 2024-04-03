4f42ed420cc1f77feb78f4e8aa1483ae3fba5a7f
aldiar1337 <vitugov2005@gmail.com>
2024-04-03 16:48:34 +0700

    3Functia

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,5 +5,29 @@ using System.Windows;
 
 namespace WpfApp1
 {
-    
+    public partial class MainWindow : Window
+    {
+        List<int> numbers = new List<int>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            private void SortAscending_Click(object sender, RoutedEventArgs e)
+            {
+                numbers.Sort();
+                UpdateListBox();
+            }
+
+            // Отсортировать по убыванию
+            private void SortDescending_Click(object sender, RoutedEventArgs e)
+            {
+                numbers.Sort();
+                numbers.Reverse();
+                UpdateListBox();
+            }
+        }
+    }
+
 }
